182fe77369a4736b6a4394d02898766ccc84838f
Ilya Shmelev <ishmelev23@gmail.com>
2018-03-14 17:23:27 +0300

    Added config vars for TIME_ZONE and LANGUAGE_CODE

--- settings/local.py
+++ settings/local.py
@@ -1,3 +1,6 @@
 from .production import *
 import django_heroku
 django_heroku.settings(locals())
+
+TIME_ZONE = os.environ.get("TIME_ZONE", "UTC")
+LANGUAGE_CODE = os.environ.get("LANGUAGE_CODE", "en-us")
