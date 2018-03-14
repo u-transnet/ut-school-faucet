715a7562dfa61d826399d4ed5a0ee2863d257410
Ilya Shmelev <ishmelev23@gmail.com>
2018-03-14 20:36:26 +0300

    Configure logging

--- settings/local.py
+++ settings/local.py
@@ -1,6 +1,47 @@
 from .production import *
 import django_heroku
-django_heroku.settings(locals())
+django_heroku.settings(locals(), logging=False)
 
 TIME_ZONE = os.environ.get("TIME_ZONE", "UTC")
 LANGUAGE_CODE = os.environ.get("LANGUAGE_CODE", "en-us")
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+        },
+        'faucet': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    }
+}
